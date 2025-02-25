ffdbf4631f6420112862a2a80d4656a3ab03a6ad
Kotatsu <kotatsu.0550@gmail.com>
2025-02-25 18:29:08 +0100

    change lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,9 +3,10 @@ return {
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
         require('lualine').setup({
-            options = { 
-                theme = 'tokyonight' 
-            }  
+            options = {
+                theme = 'tokyonight',
+                section_separators = {'', ''},
+            }
         })
     end
 }
