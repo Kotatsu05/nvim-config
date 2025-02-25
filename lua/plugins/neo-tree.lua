e8b9ae53ac39f42b7628f209840628daec663c31
Kotatsu <kotatsu.0550@gmail.com>
2025-02-25 17:25:20 +0100

    show hidden files Neotree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,6 +7,15 @@ return {
       "MunifTanjim/nui.nvim",
     },
     config = function()
+      require("neo-tree").setup({
+        filesystem = {
+          filtered_items = {
+            visible = true, -- Show hidden files and directories
+            hide_dotfiles = false, -- Set to false to show dotfiles (hidden files)
+            hide_gitignored = false, -- Set to false to show gitignored files
+          },
+        },
+      })
       vim.keymap.set('n','<C-n>', ':Neotree filesystem toggle position=right<CR>', {})
     end
 }
